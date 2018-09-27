81bee7be6c427f80103794b466d30ca6143b2049
Katia <klucas32@gatech.edu>
2018-09-27 11:39:28 -0400

    Added to the Registration class: just added findViewById(R.id.widgetHere)

--- app/src/main/java/com/capybaras/donationtracker/controller/Registration.java
+++ app/src/main/java/com/capybaras/donationtracker/controller/Registration.java
@@ -31,17 +31,26 @@ public class Registration extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
 
+        this.passwordInputText = (EditText) findViewById(R.id.passwordInputText);
+        this.reenterPasswordInputText = (EditText) findViewById(R.id.reenterPasswordInputText);
+        this.nameTextInputPlainText = (EditText) findViewById(R.id.nameTextInputPlainText);
+        this.emailInputText = (EditText) findViewById(R.id.emailInputText);
 
+        this.registerButton = (Button) findViewById(R.id.registerButton);
+        //Make on click listener
+
+        this.cancelButton = (Button) findViewById(R.id.cancelButton);
+        //make on click listener
 
 
     }
