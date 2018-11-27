0e8aa28918068c5bdfd312267a416bffc099e82b
Katia <klucas32@gatech.edu>
2018-11-26 19:37:52 -0500

    Specified the TODO comment in the Main Activity

--- app/src/main/java/com/capybaras/donationtracker/controllers/MainActivity.java
+++ app/src/main/java/com/capybaras/donationtracker/controllers/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
         file = new File(this.getFilesDir(), DataManagementFacade.ITEMS_FILE_NAME);
         dmf.loadItemText(file);
-        //TODO Uncomment these files
+        //TODO Uncomment these files (First 2 lines, Last 2 were commented out before)
 //        file = new File(this.getFilesDir(), DataManagementFacade.USERS_FILE_NAME);
 //        dmf.loadUserText(file);
 //        file = new File(this.getFilesDir(), DataManagementFacade.LOCATIONS_FILE_NAME);
